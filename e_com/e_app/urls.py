1f3df3f1888ff01fea7b711fd61f1bc68a2222ac
Dhanushya V S <75406996+Dhanushya77@users.noreply.github.com>
2024-11-13 17:50:04 +0530

    e com web add product & styling

--- e_com/e_app/urls.py
+++ e_com/e_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('',views.e_com_login),
     path('logout',views.e_com_logout),
     path('shop_home',views.shop_home),
+    path('add_pro',views.add_pro),
 ]
+
+
+    
--- e_com/e_app/views.py
+++ e_com/e_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from.models import product
 
 # Create your views here.
 
@@ -12,13 +14,33 @@ def e_com_login(req):
             login(req,shop)
             return redirect(shop_home)
         else:
+            messages.warning(req,'Invalid username or password')
             return redirect(e_com_login)
     else:
         return render(req,'login.html')
-
+# ----------------admin----------------------------------------
 def e_com_logout(req):
     logout(req)
     return redirect(e_com_login)
 
 def shop_home(req):
-    return render(req,'shop/home.html')
+    products = product.objects.all()
+    return render(req,'shop/home.html',{'product':products})
+
+def add_pro(req):
+    if req.method == 'POST':
+        pid = req.POST['pid']
+        name = req.POST['name']
+        dis = req.POST['dis']
+        price = req.POST['price']
+        offer_price = req.POST['offer_price']
+        stock = req.POST['stock']
+        img = req.POST['img']
+      
+        data = product.objects.create(pid=pid,name=name,dis=dis,price=price,offer_price=offer_price,stock=stock,img=img)
+        data.save()
+        return redirect(add_pro)
+    else:
+        data = product.objects.all()
+        return render(req,'shop/add_pro.html',{'pro':data})
+# ------------------user------------------------------------------
