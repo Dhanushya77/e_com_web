0e615a3cc41b80e46d6644cb5b577b30c6fd128b
Dhanushya V S <75406996+Dhanushya77@users.noreply.github.com>
2024-11-18 16:36:48 +0530

    e_com_web user side

--- e_com/e_app/urls.py
+++ e_com/e_app/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # --------shop-------------------
     path('',views.e_com_login),
     path('logout',views.e_com_logout),
     path('shop_home',views.shop_home),
     path('add_pro',views.add_pro),
     path('edit_pro/<id>',views.edit_pro),
+    path('delete_pro/<pid>',views.delete_pro),
+    # --------user---------------------
+    path('register',views.register),
+    path('user_home',views.user_home),
     
 ]
 
--- e_com/e_app/views.py
+++ e_com/e_app/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from.models import product
+import os
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def e_com_login(req):
     if 'shop' in req.session:
         return redirect (shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
     if req.method == 'POST':
         uname = req.POST['uname']
         password = req.POST['password']
         shop = authenticate(username=uname,password=password)
         if shop:
             login(req,shop)
-            req.session['shop'] = uname
-            return redirect(shop_home)
+            if shop.is_superuser:
+                req.session['shop'] = uname
+                return redirect(shop_home)
+            else:
+                req.session['user'] = uname
+                return redirect(user_home)
         else:
             messages.warning(req,'Invalid username or password')
             return redirect(e_com_login)
@@ -77,6 +85,35 @@ def edit_pro(req,id):
     else:
         data = product.objects.get(pk=id)
         return render(req,'shop/edit_pro.html',{'data':data})
-        
+    
+def delete_pro(red,pid):
+    data=product.objects.get(pk=pid)
+    file=data.img.url
+    file=file.split('/')[-1]
+    os.remove('media/'+file)
+    data.delete()
+    return redirect(shop_home)
     
 # -----------------user---------------------------------------------
+
+def register(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        email=req.POST['email']
+        pswrd=req.POST['pswrd']
+        try:
+            data=User.objects.create_user(first_name=uname,email=email,username=email,password=pswrd)
+            data.save()
+            return redirect(e_com_login)
+        except:
+            messages.warning(req,'Email already exist')
+            return redirect(e_com_login)
+    else:
+        return render(req,'user/register.html')
+    
+def user_home(req):
+    if 'user' in req.session:
+        products = product.objects.all()
+        return render(req,'user/home.html',{'product':products})
+    else:
+        return redirect(e_com_login)
